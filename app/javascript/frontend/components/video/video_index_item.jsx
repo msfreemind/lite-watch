20219b11dd3fdf8e2a3975c3098ce0647148001b
Mark Swan <ms.freemind@gmail.com>
2021-02-24 16:55:37 -0700

    Changed video index items to contain links to video show pages

--- app/javascript/frontend/components/video/video_index_item.jsx
+++ app/javascript/frontend/components/video/video_index_item.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const VideoIndexItem = ({ video }) => {
   return (
     <li>
-      {video.thumbnailUrl}
-      {video.title}
+      <Link to={`/videos/${video.id}`}>
+        {video.thumbnailUrl}
+        {video.title}
+      </Link>
+
       {video.author}
     </li>
   );
