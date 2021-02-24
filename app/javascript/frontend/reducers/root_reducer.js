1dd5ebf23d67d9e883ae499b5857e1759aafbe8b
Mark Swan <ms.freemind@gmail.com>
2021-02-24 16:31:33 -0700

    Modified rootReducer to have all reducers return their initial state on user logout

--- app/javascript/frontend/reducers/root_reducer.js
+++ app/javascript/frontend/reducers/root_reducer.js
@@ -1,14 +1,23 @@
 import { combineReducers } from 'redux';
+import { LOGOUT_CURRENT_USER } from '../actions/session_actions'
 import entitiesReducer from './entities_reducer';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 import uiReducer from './ui_reducer';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
   errors: errorsReducer,
   ui: uiReducer
-});
+})
+
+const rootReducer = (state, action) => {
+  if (action.type === LOGOUT_CURRENT_USER) {
+    state = undefined
+  }
+
+  return appReducer(state, action)
+}
 
 export default rootReducer;
