60824b5931294c0be6987b1ea29a11b1ee68c310
Mark Swan <ms.freemind@gmail.com>
2021-02-28 16:45:29 -0700

    Modified the logout function so that it performs a reload upon successful logout

--- app/javascript/frontend/actions/session_actions.js
+++ app/javascript/frontend/actions/session_actions.js
@@ -31,7 +31,10 @@ export const login = user => dispatch => {
 
 export const logout = () => dispatch => {
   return APIUtil.logout().then(
-    () => dispatch(logoutCurrentUser()),
+    () => { 
+      dispatch(logoutCurrentUser());
+      window.location.reload(); 
+    },
     errors => dispatch(receiveErrors(errors.responseJSON))
   );
 };
