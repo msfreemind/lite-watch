c8c194a2c548e96cdf60e618d3443952bd9ab966
Mark Swan <ms.freemind@gmail.com>
2021-04-16 18:10:08 -0600

    Changed page height to account for mobile address bar. Also, added a force reload on orientation change

--- app/javascript/packs/entry.js
+++ app/javascript/packs/entry.js
@@ -20,6 +20,23 @@ document.addEventListener("DOMContentLoaded", () => {
     store = configureStore();
   }
 
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  window.onorientationchange = function() { 
+    const  orientation = window.orientation; 
+
+    switch(orientation) { 
+      case 0:
+      case 90:
+      case -90: 
+        window.location.reload(); 
+        break; 
+    } 
+  };
+
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store}/>, root);
 });
