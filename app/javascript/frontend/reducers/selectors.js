134a5efd2b2c3d71c0ad3b3893a08ab0495d2753
Mark Swan <ms.freemind@gmail.com>
2021-06-13 16:44:17 -0600

    Added comments for state selector methods

--- app/javascript/frontend/reducers/selectors.js
+++ app/javascript/frontend/reducers/selectors.js
@@ -7,6 +7,7 @@ export const userReaction = (state, videoId) => {
   const reactions = Object.values(state.entities.reactions);
   let result = null
 
+  // Get existing reaction for the current user on the specified video
   reactions.forEach(reaction => {
     if (reaction.videoId === videoId && reaction.userId === currentUserId) {
       result = reaction;
